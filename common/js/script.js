e002b251ae69134d9d09520b346f680245d0ea01
jihyelee <jihyelee@easymedia.net>
2023-07-25 21:28:44 +0900

    LOCUS-X 클론 코딩 파일 업로드

--- common/js/script.js
+++ common/js/script.js
@@ -7,9 +7,21 @@ window.onload = function() {
 
   gsap.registerPlugin(ScrollTrigger);
 
+  const subText2 = gsap.timeline()
+  .to(".section2 .sub-tit.one span", {delay: 0.3, className: "+=show"})
+  .to(".section2 .sub-tit.two span", {delay: 0.5, className: "+=show"})
+  .to(".section2 .sub-desc span", {delay: 0.7, className: "+=show"});
   const horizontalScroll = gsap.timeline()
   .to($(".section6 .section6-slide-wrap"), {duration: 0.05, x: 0})
   .to($(".section6 .section6-slide-wrap"), {x: "-71.42%"});
+
+  ScrollTrigger.create({
+    animation: subText2,
+    trigger: ".section2",
+    start: "top 80%",
+    end: "bottom bottom",
+    scrub: 1
+  });
 
   ScrollTrigger.create({
     animation: horizontalScroll,
